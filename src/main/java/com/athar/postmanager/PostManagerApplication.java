7e4aaf013afd69ad500224c8d03917c5a9c7ebc9
Mohammad Athar <atharshaikh283@gmail.com>
2025-10-28 14:50:05 +0530

    Initial commit: Spring Boot Post Manager setup

---
+++ src/main/java/com/athar/postmanager/PostManagerApplication.java
@@ -0,0 +1,14 @@
+package com.athar.postmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostManagerApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
